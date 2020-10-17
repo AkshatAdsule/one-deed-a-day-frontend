644f3da257c32b011436e317a457994fff39e1ef
akshatadsule <akshat.adsule@gmail.com>
2020-10-16 19:52:28 -0700

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import NavBarLoggedIn from './components/NavbarLoggedIn';
+
+function App() {
+  return (
+    <div className="App">
+      <NavBarLoggedIn />
+    </div>
+  );
+}
+
+export default App;
